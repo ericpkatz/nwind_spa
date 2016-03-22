05b040c29ffeec604e7b8006249caa907e0a0a05
Eric P. Katz <ericpkatz@gmail.com>
2016-03-22 09:40:54 -0400

    allow seed variable

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var db = require('./db');
 var app = require('./app');
+var seed = require('./test/server/seed');
 
 db.connect()
+  .then(function(){
+    if(process.env.SEED)
+      return seed();
+  })
   .then(function(){
     console.log('connected to db');
     var port = process.env.PORT || 3000;
